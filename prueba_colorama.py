5e03bbd61d765c4e8d700062741c8aee01d1fdc1
TheDanilore <arteagagordillol@gmail.com>
2025-01-04 14:30:42 -0500

    Subiendo

--- prueba_colorama.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore, Style, init
-
-# Inicializar colorama
-init(autoreset=True)
-
-print(Fore.RED + "Este texto debe aparecer en rojo")
-print(Fore.GREEN + "Este texto debe aparecer en verde")
-print(Fore.BLUE + "Este texto debe aparecer en azul")
-print(Style.BRIGHT + Fore.YELLOW + "Este texto es amarillo brillante")
